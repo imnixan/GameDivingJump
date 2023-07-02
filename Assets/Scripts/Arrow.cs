96956c496a6a83ae14a7e773f51d14923836aa93
imnixan <vcmvenom@gmail.com>
2023-07-02 17:21:26 +0700

    Fix arrow bug

--- Assets/Scripts/Arrow.cs
+++ Assets/Scripts/Arrow.cs
@@ -1,21 +1,26 @@
 using UnityEngine;
+using Dreamteck.Splines;
 
 public class Arrow : JumperSubscribers
 {
     private GameObject arrowObj;
+    private SplineFollower sf;
 
     private void Start()
     {
         arrowObj = transform.GetChild(0).gameObject;
+        sf = GetComponent<SplineFollower>();
     }
 
     protected override void OnJumperIdle()
     {
         arrowObj.SetActive(true);
+        sf.follow = true;
     }
 
     protected override void OnJumperJump()
     {
         arrowObj.SetActive(false);
+        sf.follow = false;
     }
 }
